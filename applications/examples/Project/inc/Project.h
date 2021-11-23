258ef97bebbc48d3f06a506c1968dcc6dc3a62aa
Tom Raeves <tom.raeves3we@gmail.com>
2021-11-23 17:43:52 +0100

    Murata merged with project

--- applications/examples/Project/inc/Project.h
+++ applications/examples/Project/inc/Project.h
@@ -11,10 +11,24 @@ osTimerId iwdgTimId;
 osTimerId batteryTimId;
 osThreadId defaultTaskHandle;
 
+osThreadId defaultTaskHandle;
+osThreadId murata_rx_processing_handle;
+osTimerId iwdgTimId;
+osTimerId loraWANTimId;
+osTimerId moduleCheckTimId;
+osMutexId txMutexId;
+osMutexId murata_rx_process_mutex_id;
+
 void batteryLevel_measurement(void const *argument);
 void StartDefaultTask(void const *argument);
 void temp_hum_measurement(void);
 void print_temp_hum(void);
+
+void IWDG_feed(void const *argument);
+void LoRaWAN_send(void const *argument);
+void check_modules(void const *argument);
+void murata_process_rx_response(void const *argument);
+void Dualstack_ApplicationCallback(void);
 
 #ifdef __cplusplus
 }
--- applications/examples/Project/src/Project.c
+++ applications/examples/Project/src/Project.c
@@ -1,6 +1,7 @@
 #include "Project.h"
 #include "sht31.h"
 #include "stc3115.h"
+#include "murata.h"
 
 #define temp_hum_timer    3
 
@@ -9,6 +10,12 @@
 
 #define LORAWAN_INTERVAL        60   //seconds
 #define MODULE_CHECK_INTERVAL   3600 //seconds
+
+//Murata Code
+uint16_t LoRaWAN_Counter = 0;
+uint8_t murata_init = 0;
+uint64_t short_UID;
+uint8_t murata_data_ready = 0;
 
 //Battery Level Code
 STC3115_ConfigData_TypeDef STC3115_ConfigData;
@@ -22,15 +29,37 @@ int main(void)
 {
   Initialize_Platform();
 
-  // Battery monitoring
+// Battery monitoring
   GasGauge_Initialization(&common_I2C, &STC3115_ConfigData, &STC3115_BatteryData);
+//
 
-  //SHT (van temp/hum)
+//SHT (van temp/hum)
   setI2CInterface_SHT31(&common_I2C);
   SHT31_begin();
 
   osThreadDef(defaultTask, StartDefaultTask, osPriorityLow, 0, 128);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+//
+
+//Lorawan Code
+murata_init = Murata_Initialize(short_UID); //short_UID = devEUI
+UART_SetApplicationCallback(&Dualstack_ApplicationCallback, (uint8_t)MURATA_CONNECTOR);
+
+if (murata_init)
+{
+  printINF("Murata dualstack module init OK\r\n\r\n");
+}
+
+// TX MUTEX ensuring no transmits are happening at the same time
+osMutexDef(txMutex);
+txMutexId = osMutexCreate(osMutex(txMutex));
+// Thread gemaakt voor murata acties
+osThreadDef(murata_rx_processing, murata_process_rx_response, osPriorityNormal, 0, 512);
+murata_rx_processing_handle = osThreadCreate(osThread(murata_rx_processing), NULL);
+
+// pass processing thread handle to murata driver
+Murata_SetProcessingThread(murata_rx_processing_handle);
+//
 
 
   //feed IWDG every 5 seconds
@@ -38,6 +67,10 @@ int main(void)
   osTimerDef(iwdgTim, IWDG_feed);
   iwdgTimId = osTimerCreate(osTimer(iwdgTim), osTimerPeriodic, NULL);
   osTimerStart(iwdgTimId, IWDG_INTERVAL * 1000);
+
+  osTimerDef(loraWANTim, LoRaWAN_send);
+  loraWANTimId = osTimerCreate(osTimer(loraWANTim), osTimerPeriodic, NULL);
+  osTimerStart(loraWANTimId, LORAWAN_INTERVAL * 1000);
 
   osTimerDef(batteryLevel_Tim, batteryLevel_measurement);
   batteryTimId = osTimerCreate(osTimer(batteryLevel_Tim), osTimerPeriodic, NULL);
@@ -47,6 +80,13 @@ int main(void)
   temp_hum_timer_id = osTimerCreate(osTimer(temp_hum_Tim), osTimerPeriodic, NULL);
   osTimerStart(temp_hum_timer_id, temp_hum_timer * 1000);
 
+  osTimerDef(moduleCheckTim, check_modules);
+  moduleCheckTimId = osTimerCreate(osTimer(moduleCheckTim), osTimerPeriodic, NULL);
+  osTimerStart(moduleCheckTimId, MODULE_CHECK_INTERVAL * 1000);
+
+
+//Join before starting the kernel
+  Murata_LoRaWAN_Join();
 
   osKernelStart();
 
@@ -80,8 +120,8 @@ void temp_hum_measurement(void)
 
 void print_temp_hum(void)
 {
-  printINF("Temperature: %.2f degC \r\n", SHTData[0]);
-  printINF("Humidity: %.2f %% \r\n", SHTData[1]);
+  print("Temperature: %.2f degC \r\n", SHTData[0]);
+  print("Humidity: %.2f %% \r\n", SHTData[1]);
 }
 
 void StartDefaultTask(void const *argument)
@@ -92,7 +132,81 @@ void StartDefaultTask(void const *argument)
   }
 }
 
-// Murata Code
+// Lorawan 
+void LoRaWAN_send(void const *argument)
+{
+  if (murata_init)
+  {
+    uint8_t loraMessage[5];
+    uint8_t i = 0;
+    //uint16 counter to uint8 array (little endian)
+    //counter (large) type byte
+    loraMessage[i++] = 0x14;
+    loraMessage[i++] = LoRaWAN_Counter;
+    loraMessage[i++] = LoRaWAN_Counter >> 8;
+    osMutexWait(txMutexId, osWaitForever);
+    if(!Murata_LoRaWAN_Send((uint8_t *)loraMessage, i))
+    {
+      murata_init++;
+      if(murata_init == 10)
+        murata_init == 0;
+    }
+    else
+    {
+      murata_init = 1;
+    }
+    //BLOCK TX MUTEX FOR 3s
+    osDelay(3000);
+    osMutexRelease(txMutexId);
+    LoRaWAN_Counter++;
+  }
+  else{
+    printINF("murata not initialized, not sending\r\n");
+  }
+}
 
-//
+void check_modules(void const *argument)
+{
+  printINF("checking the status of the modules\r\n");
+  if (!murata_init)
+  {
+    // LORAWAN
+    murata_init = Murata_Initialize(short_UID);
+    Murata_toggleResetPin();
+  }
+}
+
+void murata_process_rx_response(void const *argument)
+{
+  uint32_t startProcessing;
+  while (1)
+  {
+    // Wait to be notified that the transmission is complete.  Note the first
+    //parameter is pdTRUE, which has the effect of clearing the task's notification
+    //value back to 0, making the notification value act like a binary (rather than
+    //a counting) semaphore.
+    startProcessing = ulTaskNotifyTake(pdTRUE, osWaitForever);
+    if (startProcessing == 1)
+    {
+      // The transmission ended as expected.
+      while(murata_data_ready)
+      {
+        printINF("processing murata fifo\r\n");
+        murata_data_ready = !Murata_process_fifo();
+        osDelay(50);
+      }
+    }
+    else
+    {
+    }
+    osDelay(1);
+  }
+  osThreadTerminate(NULL);
+}
+
+void Dualstack_ApplicationCallback(void)
+{
+  murata_data_ready = 1;
+}
+
 
