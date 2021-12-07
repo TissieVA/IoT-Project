c864ddc01d2640fb84aef1743489322a8644a215
Wouter <wouter.huygen@hotmail.be>
2021-12-07 18:01:28 +0100

    Data now loaded from backend api

--- mobile-app/frost-detector/src/app/tab2/tab2.page.ts
+++ mobile-app/frost-detector/src/app/tab2/tab2.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  sensor_data: any = [];
+
+  ngOnInit(){
+    this.runHttp();
+  }
+
+  runHttp() {
+    this.http.get('http://143.129.80.197:8080/all-sensor-data')
+      .subscribe(data => {
+        console.log(data);
+        this.sensor_data = data;
+      });
+  }
 }
